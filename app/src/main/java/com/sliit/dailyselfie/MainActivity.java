48ffd5ce9a7def43f52d7bdafb0b84cb0899e291
Tharaka <tharakaromesh@gmail.com>
2016-03-15 09:42:14 +0530

    second commit

--- app/src/main/java/com/sliit/dailyselfie/MainActivity.java
+++ app/src/main/java/com/sliit/dailyselfie/MainActivity.java
@@ -83,6 +83,17 @@ public class MainActivity extends BaseActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
 
-
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
+            findViewById(R.id.activity_MainMenu).setPadding(72, 0, 72, -250);
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
+            findViewById(R.id.activity_MainMenu).setPadding(16, 16, 16, 16);
+        }
+    }
 }
