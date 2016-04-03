2ed1386b33a452ff6546cf2e58577d88709b7d17
Tharaka <tharakaromesh@gmail.com>
2016-04-03 10:20:40 +0530

    new navgation drawer

--- app/src/main/java/com/sliit/dailyselfie/FitnessActivity.java
+++ app/src/main/java/com/sliit/dailyselfie/FitnessActivity.java
@@ -1,47 +1,35 @@
 package com.sliit.dailyselfie;
 
-import android.app.ActionBar;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
-import android.view.Gravity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.NumberPicker;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.vi.swipenumberpicker.OnValueChangeListener;
 import com.vi.swipenumberpicker.SwipeNumberPicker;
 
-public class FitnessActivity extends BaseActivity {
+public class FitnessActivity extends AppCompatActivity {
 
     SwipeNumberPicker fitpicker;
     SwipeNumberPicker fitpicker1;
     SwipeNumberPicker fitpicker2;
     SwipeNumberPicker fitpicker3;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fitness);
-
-
-
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         findViewById(R.id.setAlarm).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -111,8 +99,9 @@ public class FitnessActivity extends BaseActivity {
                 return true;
             }
         });
+
+
+
     }
-
-
 
 }
--- app/src/main/java/com/sliit/dailyselfie/MainActivity.java
+++ app/src/main/java/com/sliit/dailyselfie/MainActivity.java
@@ -3,15 +3,9 @@ package com.sliit.dailyselfie;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ScrollView;
 import android.widget.Toast;
 
 public class MainActivity extends BaseActivity {
