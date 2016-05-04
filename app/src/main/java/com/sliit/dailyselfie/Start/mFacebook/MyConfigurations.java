1d97480681a04551d551cca688cf64b70b7af7a4
Tharaka <tharakaromesh@gmail.com>
2016-05-04 12:38:32 +0530

    fb logging added

---
+++ app/src/main/java/com/sliit/dailyselfie/Start/mFacebook/MyConfigurations.java
@@ -0,0 +1,24 @@
+package com.sliit.dailyselfie.Start.mFacebook;
+
+import com.sromku.simple.fb.Permission;
+import com.sromku.simple.fb.SimpleFacebookConfiguration;
+
+/**
+ * Created by Tharaka on 04/05/2016.
+ */
+public class MyConfigurations {
+
+    Permission[] permissions = new Permission[]{Permission.EMAIL};
+    static final String APP_ID="1529806223987699";
+
+    public SimpleFacebookConfiguration getMyConfigs(){
+
+        SimpleFacebookConfiguration configuration = new SimpleFacebookConfiguration.Builder()
+                .setAppId(APP_ID)
+                .setNamespace("facebookstart")
+                .setPermissions(permissions)
+                .build();
+        return  configuration;
+    }
+
+}
