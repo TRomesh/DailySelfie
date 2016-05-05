9901fd651bc192c4ac3594adbfb8f0d7446abf9b
MadushikaPerera <madushikad.perera@gmail.com>
2016-05-05 15:03:30 +0530

    main part done

--- app/src/main/java/com/sliit/dailyselfie/Start/SplashActivity.java
+++ app/src/main/java/com/sliit/dailyselfie/Start/SplashActivity.java
@@ -1,18 +1,27 @@
 package com.sliit.dailyselfie.Start;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 
+import com.sliit.dailyselfie.MainActivity;
 import com.sliit.dailyselfie.R;
 
 public class SplashActivity extends AppCompatActivity {
+
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        SharedPreferences userDetails = getSharedPreferences("userDetails", Context.MODE_PRIVATE);
+        userID = userDetails.getString("loggedUserId","");
+
         new CountDownTimer(3000, 1000) {
 
             public void onTick(long millisUntilFinished) {
@@ -20,12 +29,18 @@ public class SplashActivity extends AppCompatActivity {
             }
 
             public void onFinish() {
-                Intent i=new Intent(SplashActivity.this,LoginActivity.class);
-                startActivity(i);
-                finish();
+                if(userID == ""){
+                    Intent i=new Intent(SplashActivity.this,LoginActivity.class);
+                    startActivity(i);
+                    finish();
+                } else {
+                    Intent i=new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+
             }
         }.start();
-
 
     }
 
